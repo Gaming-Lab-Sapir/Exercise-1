fd65e3c6096cc502bc81ac189ad13ab3136d0c7c
michael_miron <micha767a@gmail.com>
2025-08-18 16:44:04 +0300

    add simple Collectable coin prefab with trigger detection

--- Assets/Scripts/Collectable.cs
+++ Assets/Scripts/Collectable.cs
@@ -2,15 +2,30 @@ using UnityEngine;
 
 public class Collectable : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private SpriteRenderer sprite;
+    private bool playerNotReached = false;
+    private bool playerTouched = true;
+
     void Start()
     {
-        
+        sprite = GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
         
     }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        playerNotReached = true;
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("player touched the coin");
+            playerTouched = true;
+            sprite.enabled = false;
+            Destroy(gameObject, 0.5f);//right now I'm not sure if it should be destroyed or not, we'll see later when doing the pick up logic.
+        }
+    }
+
 }
